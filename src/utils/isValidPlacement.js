8fcdfe26e451d9f74ec2be9403692aed4af3195d
Tim Weightman <Tim.Weightman@myob.com>
2017-12-08 23:18:36 +1100

    add integer parsing to isValidPlacement for safety

--- src/utils/isValidPlacement.js
+++ src/utils/isValidPlacement.js
@@ -1,8 +1,10 @@
 import facing from '../config/facing';
 
 export default (table, x, y, f) => {
-  const isValidX = x >= 0 && x < table.columns;
-  const isValidY = y >= 0 && y < table.rows;
+  const parsedX = parseInt(x, 10);
+  const parsedY = parseInt(y, 10);
+  const isValidX = parsedX >= 0 && parsedX < table.columns;
+  const isValidY = parsedY >= 0 && parsedY < table.rows;
   const isValidFacing = !!facing[f];
 
   return isValidX && isValidY && isValidFacing;
